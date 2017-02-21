f835ef1c837692a989b16a7e0eb0b8e9008a4a4a
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-21 23:21:11 +0530

    connected user schema

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import User from '../models/user';
 
 const router = express.Router();
 
 router.route('/')
-	.get((req, res) => res.send('users'))
+	.get((req, res) => {
+		User.find({}, (err, users) => {
+			if(err)
+				res.send(err)
+			res.json(users);
+		});
+	})
 
 export default router;
