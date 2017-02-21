68b9eaa577f4e2717b382710692bb4cc3bc8d717
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-21 23:06:05 +0530

    imported all routes

--- app/server.js
+++ app/server.js
@@ -2,12 +2,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import * as constants from './constants'; // import constants
+import routes from './routes';
 
 const app = express(); // new server
 
 // parse body params
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', routes);
 
 // start app on PORT
 app.listen(constants.PORT, () => console.log(`Started server on ${constants.PORT}`));
