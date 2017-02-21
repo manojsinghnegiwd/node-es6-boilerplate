13ff3435ef6524009941171ae02bdef62d076186
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-21 23:20:40 +0530

    added ports

--- app/constants.js
+++ app/constants.js
@@ -1,5 +1,9 @@
 const PORT = 3000; // port to start server on
+const MONGO_PORT = 27017; // mongo port
+const DB_NAME = 'sample' // sample db name
 
 export {
-	PORT
+	PORT,
+	MONGO_PORT,
+	DB_NAME
 }
