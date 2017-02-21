3e3932c65dc9240def934fe7354dfc0918a01acd
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-21 23:20:19 +0530

    connected mongoose

--- app/server.js
+++ app/server.js
@@ -1,10 +1,13 @@
 // import express
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import * as constants from './constants'; // import constants
 import routes from './routes';
 
 const app = express(); // new server
+
+mongoose.connect(`mongodb://localhost:${constants.MONGO_PORT}/${constants.DB_NAME}`);
 
 // parse body params
 app.use(bodyParser.json());
