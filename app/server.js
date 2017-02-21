0664eefcab7ffdeb50b1e70c770aa4ad45cd2f1e
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-21 22:46:14 +0530

    added bodyParser

--- app/server.js
+++ app/server.js
@@ -1,8 +1,13 @@
 // import express
 import express from 'express';
+import bodyParser from 'body-parser';
 import * as constants from './constants'; // import constants
 
 const app = express(); // new server
+
+// parse body params
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // start app on PORT
 app.listen(constants.PORT, () => console.log(`Started server on ${constants.PORT}`));
